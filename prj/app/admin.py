51b137b64d8b4277aaf3ec152820374d6041d6f5
school <filip.dostal.s@p1-7.ad.gyarab.cz>
2026-03-20 12:16:00 +0100

    Added models.py

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Food, Meal
 
-# Register your models here.
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'calories', 'protein', 'carbs', 'fats')
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('user', 'food', 'amount', 'date')
--- prj/app/models.py
+++ prj/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    calories = models.FloatField()
+    protein = models.FloatField()   
+    carbs = models.FloatField()    
+    fats = models.FloatField()     
+    sugar = models.FloatField(null=True)
+    saturated_fats = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.name
+
+class Meal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    amount = models.FloatField() 
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.food.name} ({self.date})"
