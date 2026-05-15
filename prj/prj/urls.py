10aa970603670741f8f774407bfaad0ef5af897a
school <filip.dostal.s@p1-7.ad.gyarab.cz>
2026-05-15 12:15:08 +0200

    Add API playground page and link in navigation

--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.render_home, name="home"),
     path('about/', views.render_about, name="about"),
+    path('api_playground/', views.render_api_playground, name="api_playground"),
     path("api/", api.urls)
 ]
