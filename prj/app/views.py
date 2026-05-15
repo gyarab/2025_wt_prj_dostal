ab201adafd23a0e1e34dccfa5ab28a58f89ed1a8
Filip Dostal <filip.dostal.s@gyarab.cz>
2026-05-16 01:02:08 +0200

    Updated ui and finished api playground homework

--- prj/app/views.py
+++ prj/app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from .models import Food
 
 def render_home(request):
     return render(request, 'home.html')
-
-def render_about(request):
-    return render(request, 'about.html')
+def render_food_database(request):
+    search_query = request.GET.get('q', '')
+    if search_query:
+        foods = Food.objects.filter(name__icontains=search_query)
+    else:
+        foods = Food.objects.all()
+    
+    return render(request, 'food_database.html', {'foods': foods, 'search_query': search_query})
 
 def render_api_playground(request):
     return render(request, 'api_playground.html')
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -7,7 +7,7 @@ from app.api import api
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.render_home, name="home"),
-    path('about/', views.render_about, name="about"),
+    path('food_database/', views.render_food_database, name="food_database"),
     path('api_playground/', views.render_api_playground, name="api_playground"),
     path("api/", api.urls)
 ]
